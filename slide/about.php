b4a4843ae0b3a8cc85c7045c6e47900d6c82e412
firmansyahswp <116624697+firmansyahswp@users.noreply.github.com>
2022-12-21 22:01:43 +0700

    Create about.php

---
+++ slide/about.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--divinectorweb.com-->
+<head>
+    <meta charset="UTF-8">
+    <title>	Profile</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="../library/style.css">
+</head>
+<body>
+<div class="container-about bg-dark">
+<nav class="navbar navbar-expand-lg opacity fixed-top fs-4">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse navbar-slide" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link text-light" aria-current="page" href="../">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link text-light" aria-current="page" href="ft.php">FT</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link text-light" aria-current="page" href="#">Proyektor</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link text-warning" href="about.php">About</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    
+<div class="team-area">
+  <div class="container">
+  <div class="header">
+	<p> 
+  </div>
+    <div class="row">
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="single-team">
+          <div class="img-area">
+            <img src="../media/umam.jpg" class="img-responsive" alt="">
+            <div class="social">
+			  <p> My social media</p>
+              <ul class="list-inline">
+                 <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+				 <li><a href="https://www.instagram.com/ryuriko_/"><i class="fa fa-instagram"></i></a></li>
+				 <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				 <li><a href="github.com/Ryuriko"><i class="fa fa-github"></i></a></li>
+              </ul>
+			  <p> Hallo Everyone!</p>
+            </div>
+          </div>
+          <div class="img-text">
+            <h4>Ahmad Khoirul Umam</h4>
+			<h4>3337210006</h4>
+            <h5>Founder, Developer & Designer</h5>
+          </div>
+        </div>
+      </div>
+	  
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="single-team">
+          <div class="img-area">
+            <img src="https://i.ibb.co/wcg9K0M/IMG20210407221948-1.jpg" class="img-responsive" alt="IMG20210407221948-1">
+			<div class="social">
+			<p> My Social Media </p>
+              <ul class="list-inline">
+                <li><a href="https://www.facebook.com/firmansyahswp/"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="https://www.instagram.com/firmans43_/"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="https://wa.me//+6289501386022/"><i class="fa fa-whatsapp"></i></a></li>
+                <li><a href="github.com/firmansyahswp"><i class="fa fa-github"></i></a></li>
+              </ul>
+			  <p> Hello my name is Firmansyah i from tangerang nice to meet you , follow my social media dont forget guys :v </p>
+            </div>
+          </div>
+          <div class="img-text">
+            <h4>Firmansyah Sutan Wahyu Prakosa</h4>
+			<h4>3337210030</h4>
+            <h5>Developer & Designer</h5>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="single-team">
+          <div class="img-area">
+            <img src="https://bnz06pap001files.storage.live.com/y4mmYEcuaIwDTSJqDUKuyECWSqL_-t9Im7kDW31gy5hJNO5bDzw2ONoIOYkxxXUjgRWZ4afFQ7Z9GhEKcF-1TO0dK-tILAWpvyd0a2DCOBRZa-JNqgxNtX7Dq90ymOqbqrB1Bi9KjkqwO2aubhOdCsiwGis64WfjJmCHjiUneHmVH04Q4UCkxnViHNpXC7BW6gx?width=900&height=900&cropmode=none" class="img-responsive" alt="">
+            <div class="social">
+				<p> My Social Media</p> 
+              <ul class="list-inline">
+                <li><a href="https://www.linkedin.com/in/anjeellun-783a2b104"><i class="fa fa-linkedin"></i></a></li>
+				<li><a href="https://www.instagram.com/anjeellun/"><i class="fa fa-instagram"></i></a></li>
+				<li><a href="https://twitter.com/vamlienpsycho?t=3hZ1gyes6t8qCLZvyY8u9A&s=09"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="https://github.com/Anjeellun"><i class="fa fa-github"></i></a></li>>
+              </ul>
+			  <p> Hello~! My name is Anjeli Gusnawan but you all can call me Jel. Imma student at University Sultan Ageng Tirtayasa majoring in Informatics. And I interested in becoming a Front-end developer or web/application designer!</p>
+            </div>
+          </div>
+          <div class="img-text">
+            <h4>Anjeli Gusnawan</h4>
+			<h4>3337210040</h4>
+            <h5>Developer & Designer</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+	<div class="row">
+	<div class="col-md-4 col-sm-6 col-xs-12">
+	</div>
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="single-team">
+          <div class="img-area">
+            <img src="../media/fariz.jpeg" class="img-responsive" alt="">
+            <div class="social">
+			<p> My Social Media </p>
+              <ul class="list-inline">
+                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-github"></i></a></li>
+              </ul>
+				<p>Hallo!</p>
+            </div>
+          </div>
+          <div class="img-text">
+            <h4>M Fariz Abqory</h4>
+			<h4>3337210058</h4>
+            <h5>Designer</h5>
+          </div>
+        </div>
+      </div>
+	  </div>
+  </div>
+</div>
+</div>
+</body>
+</html>
